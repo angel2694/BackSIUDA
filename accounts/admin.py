ae967aeedeab868c819134fb3cade90846707149
Angel <fisiangel.14@gmail.com>
2026-05-06 08:16:26 -0500

    add apps and master tables

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
-
-# Register your models here.
+from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
+
 @admin.register(CustomUser)
-class CustomUserAdmin(admin.ModelAdmin):
+class CustomUserAdmin(UserAdmin):
     list_display = ('username', 'email', 'role', 'is_active')
-    list_filter = ('role','is_active')
+    list_filter = ('role', 'is_active')
     search_fields = ('username', 'email')
+    fieldsets = UserAdmin.fieldsets + (
+        ('Rol', {'fields': ('role',)}),
+    )
