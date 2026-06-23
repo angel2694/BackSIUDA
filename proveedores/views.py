0b0218e087ec6fa87601b69fd956eebd8ad7caf7
Angel <fisiangel.14@gmail.com>
2026-06-23 09:07:34 -0500

    HU31 - add Backend

--- proveedores/views.py
+++ proveedores/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import Supplier
+from .serializers import SupplierSerializer
+from accounts.permissions import IsAdmin
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+class SupplierViewSet(ModelViewSet):
+    queryset = Supplier.objects.all().order_by('id')
+    serializer_class = SupplierSerializer
+    permission_classes = [IsAuthenticated, IsAdmin]
