cf810cf73016b8bba4fb8f266c33eca6bb07870d
Angel <fisiangel.14@gmail.com>
2026-06-24 09:40:23 -0500

    fiex bug about graphics2

--- areas/views.py
+++ areas/views.py
@@ -2,9 +2,10 @@ from rest_framework.viewsets import ModelViewSet
 from accounts.permissions import IsAdminOrReadOnly
 from .models import Area
 from .serializers import AreaSerializer
-
+from rest_framework.permissions import IsAuthenticated
+from accounts.permissions import IsAdmin
 class AreaViewSet(ModelViewSet):
     queryset = Area.objects.all().order_by('id')
     serializer_class = AreaSerializer
     permission_classes = [IsAdminOrReadOnly]
-    permission_classes = [IsAuthenticated, IsAdmin]
+    # permission_classes = [IsAuthenticated, IsAdmin]
